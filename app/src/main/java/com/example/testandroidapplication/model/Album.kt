ebda206951a0203b613070fb487ff83b37ffcf2e
ChawkiChaouachi <chawachi.chawki.ing@gmail.com>
2019-04-10 12:41:31 +0200

    Update Album.kt
     define set fields as entities
    
    Co-Authored-By: chawkichaouachi <chawkichaouachi@users.noreply.github.com>

--- app/src/main/java/com/example/testandroidapplication/model/Album.kt
+++ app/src/main/java/com/example/testandroidapplication/model/Album.kt
@@ -1,9 +1,21 @@
 package com.example.testandroidapplication.model
 
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(
+    tableName = "albums"
+)
 data class Album (
+    @ColumnInfo
     val albumId : Int,
+    @PrimaryKey
     val id : Int,
+    @ColumnInfo
     val title : String,
+    @ColumnInfo
     val url :String,
+    @ColumnInfo
     val thumbnailUrl : String
 )
