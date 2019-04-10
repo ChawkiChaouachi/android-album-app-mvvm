7ca1f34be03afd78e85d29fd26939e319a1b39b8
ChawkiChaouachi <chawachi.chawki.ing@gmail.com>
2019-04-10 11:14:24 +0200

    Create AdapterAlbumsRecycler.kt
    Create Adapter For RecyclerView
    
    Co-Authored-By: chawkichaouachi <chawkichaouachi@users.noreply.github.com>

---
+++ app/src/main/java/com/example/testandroidapplication/ui/adapters/AdapterAlbumsRecycler.kt
@@ -0,0 +1,42 @@
+package com.example.testandroidapplication.ui.adapters
+
+import android.databinding.DataBindingUtil
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.testandroidapplication.R
+import com.example.testandroidapplication.databinding.SingleItemAlbumLayoutBinding
+import com.example.testandroidapplication.model.Album
+import com.example.testandroidapplication.ui.PostSingleAlbulViewModel
+
+class AdapterAlbumsRecycler : RecyclerView.Adapter<AdapterAlbumsRecycler.ViewHolder>() {
+    private lateinit var listAlbum : List<Album>
+
+
+    override fun onCreateViewHolder(parent : ViewGroup, p1: Int): ViewHolder {
+        val itemNumberBinding : SingleItemAlbumLayoutBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.single_item_album_layout,parent,false);
+        return ViewHolder(itemNumberBinding);
+    }
+
+    override fun getItemCount(): Int {
+        return if(::listAlbum.isInitialized) listAlbum.size else 0
+    }
+
+    override fun onBindViewHolder(viewholder: ViewHolder, position: Int) {
+        viewholder.bind(listAlbum[position])
+    }
+
+    fun updateChanged(notifiedListAlbum: List<Album>){
+        listAlbum = notifiedListAlbum
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(private val binding: SingleItemAlbumLayoutBinding): RecyclerView.ViewHolder(binding.root){
+        private val viewModel = PostSingleAlbulViewModel()
+
+        fun bind(album: Album){
+            viewModel.bind(album)
+            binding.viewModel = viewModel
+        }
+    }
+}
